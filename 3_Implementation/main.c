6bfdea26a4df20ecdeaf39de0ab9dcb205ce3758
SFID-264673 <80456607+isha121298@users.noreply.github.com>
2021-04-16 12:03:58 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -4,43 +4,43 @@
 #include"functions.h"
 int main()
 {
-    displaymenu();
+    displaymenu();   /// display the menus for the game
     char name[30];
     char option[3];
-    scanf("%s",&option);
+    scanf("%s",&option);  /// Scanning the option according to the display menu
 
-    if(option=="S")
+    if(option=="S")  /// If 'S' is selected then start the game
     {
         printf("Enter Name");
         gets(name);
-        displayrules();
-        if(getchar()=='Y')
+        displayrules();  /// display the rules for the game and ask to continue or quit
+        if(getchar()=='Y')   /// If 'Y' then start the practice game
         {
-            startpracticegame();
-            displayoption();
-            if(getchar()=='C')
+            startpracticegame(); ///running the practice game
+            displayoption();  /// Choose to continue or to quit
+            if(getchar()=='C') /// Continue if 'C' is choosen
             {
-                startgame();
+                startgame(); /// running the main game and printing the score after each question.
             }
             else{displaymenu();}
         }
-        else if(getchar()=='N')
+        else if(getchar()=='N') ///if 'N' is choosen then it goes back to the display menu
         {
-            displaymenu();
+            displaymenu(); ///Displaying the menu
         }
         else
         {
-            printf("\n Invalid Press!");
+            printf("\n Invalid Press!");  /// If any wrong option is given it will show Invalid Press
         }
 
     }
-    else if(option=="Q")
+    else if(option=="Q") /// If 'Q' is chooses then it will quit the program
     {
-        
+        exit();  /// Terminates the programs 
     }
     else
     {
-        printf("\n Invalid Press!");
+        printf("\n Invalid Press!"); /// If any other option is choose it will show Invalid Press
     }
     return 0;
 }
